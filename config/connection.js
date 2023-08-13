42d711cc77954bccb77fd71420b6c2d9f7d1b9d2
unknown <arundvp@gmail.com>
2023-08-12 23:21:28 -0700

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config(); // Import sensitive data from .env
 const sequelize = process.env.JAWSDB_URL 
   ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL is for HEROKU
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // This is for Local
-      host: 'localhost',
+      host: process.env.DB_HOST,
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
